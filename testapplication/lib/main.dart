838f16635e47f23c2462488425ddb52fb7dd0ae8
Max Dzhun <77993867+MaxDzhun@users.noreply.github.com>
2023-02-19 09:56:04 +0200

    Initial commit

---
+++ testapplication/lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() =>runApp (MyApp());
+
+class MyApp extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home:ChangeColor()
+    );
+  }
+}
+class ChangeColor extends StatefulWidget {
+  const ChangeColor({Key? key}) : super(key: key);
+
+
+
+  @override
+  State<ChangeColor> createState() => _ChangeColorState();
+}
+
+class _ChangeColorState extends State<ChangeColor> {
+
+  Color startColor = Colors.white;
+
+  void RandomColor(){
+    setState(() {
+      startColor = Color(Random().nextInt(0xffffffff + 1));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: RandomColor,
+      child: Container(
+        color: startColor,
+        child: Center(
+          child: Text('Hey there',
+            style:TextStyle(height:0 ,fontSize:25,decoration: TextDecoration.none),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
